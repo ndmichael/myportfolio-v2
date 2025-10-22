4cc34a9777afac985c9326d29457298a333bc754
ndmichael <ukejemicheal@gmail.com>
2025-10-22 15:57:05 +0100

    added the general navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-            {children}
+          <Navbar />
+            <main>
+              {children}
+            </main>
         </ThemeProvider>
       </body>
     </html>

fd651cff674b94992d2205911173f8adb2b12dfc
ndmichael <ukejemicheal@gmail.com>
2025-11-07 21:26:06 +0100

    improved to include more media as props

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -5,24 +5,46 @@ import CaseApproach from "@/components/case-studies/CaseApproach";
 import CaseResults from "@/components/case-studies/CaseResults";
 import CaseStacks from "@/components/case-studies/CaseStacks";
 
-export default function CaseStudyPage({ params }: { params: { slug: string } }) {
-  const data = caseStudies[params.slug];
+export default async function CaseStudyPage({ params }: { params: Promise<{ slug: string }> }) {
+    
 
-  if (!data) return <div className="p-10 text-center">Case study not found.</div>;
+  const { slug } = await params; 
+  const data = caseStudies.find((item) => item.slug === slug);
+
+  if (!data) return <div className="p-10 text-center">
+    <p className="my-32">Case study not found.</p>
+  </div>;
 
   return (
     <div className="space-y-20">
-      <HeroCaseStudy title={data.title} tagline={data.tagline} media={data.media} />
+      <HeroCaseStudy 
+        title={data.title} 
+        tagline={data.tagline} 
+        media={data.media.hero}
+        />
 
-      <CaseContext role={data.role} overview={data.overview} context={data.context} />
+        <CaseContext 
+        role={data.role} 
+        overview={data.overview} 
+        context={data.context}
+        media={data.media.context}
+        />
 
-      <CaseStacks stack={data.stack} />
+        <CaseStacks stack={data.stack} />
 
-      <CaseApproach approach={data.approach} features={data.features} />
+        <CaseApproach
+            approach={data.approach}
+            features={data.features}
+            media={data.media.approach}          // ✅ Single image
+            featureMedia={data.media.features}   // ✅ Array
+            />
 
-      <CaseResults results={data.results} />
+        <CaseResults 
+        results={data.results} 
+        media={data.media.results}
+        />
 
-      {/* optional: links */}
+
       <div className="flex gap-4 pt-6">
         {data.github && (
           <a href={data.github} target="_blank" className="primary-button">
